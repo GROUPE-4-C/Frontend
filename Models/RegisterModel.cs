aaf7a1695e8ebf191d6c328eeb9556e934618304
Angedeke <angelicadeke@gmail.com>
2025-06-18 00:13:26 +0000

    modification

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,6 +1,26 @@
-﻿namespace AlumniConnect.Front.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlumniConnect.Front.Models
 {
     public class RegisterModel
     {
+        [Required(ErrorMessage = "L'e-mail est obligatoire.")]
+        [EmailAddress(ErrorMessage = "Format d'e-mail invalide.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [MinLength(6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères.")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Le nom complet est obligatoire.")]
+        public required string FullName { get; set; }
+
+        [Required(ErrorMessage = "Sélectionnez une promotion.")]
+        public required string Promotion { get; set; }
+
+        public string Profession { get; set; } = "";
+        public string PhotoUrl { get; set; } = "";
+        public string Bio { get; set; } = "";
+
     }
 }
